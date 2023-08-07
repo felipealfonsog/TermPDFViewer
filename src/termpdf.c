d2eb90e42c090e2dabf5819479694aebc9cf6135
Felipe <f.alfonso@res-ear.ch>
2023-08-07 02:07:40 -0400

    Updates

--- src/termpdf.c
+++ src/termpdf.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mupdf/fitz.h>
 
+void render_page(fz_context *ctx, fz_document *doc, int page_num, float zoom) {
+    fz_page *page = fz_load_page(ctx, doc, page_num);
+    if (!page) {
+        printf("Failed to load page %d.\n", page_num + 1);
+        return;
+    }
+
+    fz_rect bounds;
+    fz_bound_page(ctx, page, &bounds);
+    fz_matrix transform = fz_scale(zoom, zoom);
+
+    fz_device *dev = fz_new_device(ctx, &bounds);
+    fz_run_page(ctx, page, dev, &transform, NULL);
+    fz_free_device(dev);
+    fz_drop_page(ctx, page);
+}
+
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
+    if (argc != 2) {
         printf("Usage: %s <filename.pdf>\n", argv[0]);
         return 1;
     }
@@ -13,8 +31,6 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    fz_register_document_handlers(ctx);
-
     fz_document *doc = fz_open_document(ctx, argv[1]);
     if (!doc) {
         printf("Error opening the PDF file: %s\n", argv[1]);
@@ -22,43 +38,47 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    int page_count = fz_count_pages(ctx, doc); // Use 'ctx' as the first argument
+    int page_count = fz_count_pages(ctx, doc);
+    if (page_count <= 0) {
+        printf("No pages found in the PDF.\n");
+        fz_close_document(doc);
+        fz_free_context(ctx);
+        return 1;
+    }
+
     int current_page = 0;
+    float zoom = 1.0;
     char input;
 
     printf("PDF Viewer: %s\n", argv[1]);
 
     while (1) {
-        if (current_page < 0) {
-            current_page = 0;
-        } else if (current_page >= page_count) {
-            current_page = page_count - 1;
-        }
+        render_page(ctx, doc, current_page, zoom);
 
-        fz_page *page = fz_load_page(ctx, doc, current_page);
-        if (!page) {
-            printf("Failed to load the page.\n");
-            fz_close_document(doc);
-            fz_free_context(ctx);
-            return 1;
-        }
-
-        fz_rect bounds;
-        fz_bound_page(ctx, page, &bounds);
-        fz_matrix transform = fz_identity;
-
-        fz_device *dev = fz_new_device(ctx, &bounds);
-        fz_run_page(ctx, page, dev, &transform, NULL);
-        fz_free_device(dev);
-        fz_drop_page(ctx, page);
-
-        printf("\nPage %d (%g x %g) - (N)ext, (P)revious, (Q)uit: ", current_page + 1, bounds.x1 - bounds.x0, bounds.y1 - bounds.y0);
+        printf("\nPage %d/%d - Zoom: %.2f - (N)ext, (P)revious, (Z)oom, (G)o to page, (Q)uit: ",
+               current_page + 1, page_count, zoom);
         scanf(" %c", &input);
 
         if (input == 'n' || input == 'N') {
             current_page++;
+            if (current_page >= page_count) {
+                current_page = page_count - 1;
+            }
         } else if (input == 'p' || input == 'P') {
             current_page--;
+            if (current_page < 0) {
+                current_page = 0;
+            }
+        } else if (input == 'z' || input == 'Z') {
+            printf("Enter zoom factor (e.g. 1.5): ");
+            scanf("%f", &zoom);
+        } else if (input == 'g' || input == 'G') {
+            printf("Enter page number (1-%d): ", page_count);
+            int new_page;
+            scanf("%d", &new_page);
+            if (new_page >= 1 && new_page <= page_count) {
+                current_page = new_page - 1;
+            }
         } else if (input == 'q' || input == 'Q') {
             break;
         }
