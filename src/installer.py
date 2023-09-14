f69a8e7a10a2f0a21a74c128f1dec9fc503a603b
Felipe <f.alfonso@res-ear.ch>
2023-09-13 21:35:51 -0300

    Updates

--- src/installer.py
+++ src/installer.py
@@ -3,10 +3,9 @@ import platform
 import shutil
 
 def install_term_pdf():
-    # Determina el nombre del archivo de origen (termpdf.py)
-    source_file = 'termpdf.py'
+    
+    source_file = 'src/termpdf.py'
 
-    # Determina la ubicación del directorio de programas en función del sistema operativo
     if platform.system() == 'Darwin':  # macOS
         programs_directory = '/usr/local/bin'
     elif platform.system() == 'Linux':  # Linux
@@ -15,17 +14,15 @@ def install_term_pdf():
         print("Sistema operativo no soportado.")
         return
 
-    # Ruta completa al archivo de destino (el enlace simbólico)
+  
     target_file = os.path.join(programs_directory, 'term-pdf')
 
     try:
-        # Copia termpdf.py al directorio de programas
+   
         shutil.copy(source_file, programs_directory)
 
-        # Crea un enlace simbólico al archivo termpdf.py
         os.symlink(os.path.join(programs_directory, source_file), target_file)
 
-        # Dale permisos de ejecución al archivo termpdf.py
         os.chmod(target_file, 0o755)
 
         print("TermPDF Viewer ha sido instalado y se puede ejecutar usando 'term-pdf' en la línea de comandos.")
