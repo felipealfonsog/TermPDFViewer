c3dd762b321f46d559fbc53f8e59f3ce114bf01b
Felipe <f.alfonso@res-ear.ch>
2023-10-25 19:34:53 -0300

    Updates - testing new futures unstable files

--- src/termpdf_unstable.py
+++ src/termpdf_unstable.py
@@ -32,7 +32,8 @@ view and navigate PDF files directly within the terminal.
 
 * Install PyMuPDF library (if not already installed):
   pip install PyMuPDF
-  pip install keyboard
+  pip install pynput
+
   
 * Run the TermPDF Viewer:
 * python termpdf.py
@@ -56,62 +57,80 @@ view and navigate PDF files directly within the terminal.
 
 
 
+
 import os
 import fitz
-import platform
-
-# Use msvcrt only on Windows
-if platform.system() == 'Windows':
-    import msvcrt
-    getch = msvcrt.getch
-else:
-    import keyboard
-    getch = keyboard.read_event
+import curses
 
 def scan_pdf_files():
     pdf_files = [file for file in os.listdir('.') if file.lower().endswith('.pdf')]
     return pdf_files
 
-def display_pdf(pdf_filename):
+def display_current_page(stdscr, current_page, total_pages, doc):
+    if not (0 <= current_page < total_pages):
+        stdscr.addstr("Invalid page number.")
+        stdscr.refresh()
+        return
+
+    page = doc[current_page]
+    pdf_text = page.get_text()
+
     try:
-        doc = fitz.open(pdf_filename)
-        total_pages = doc.page_count
-        current_page = 0
+        stdscr.addstr(f'Page {current_page + 1} / {total_pages}\n')
+        stdscr.addstr(pdf_text)
+    except curses.error:
+        lines = pdf_text.split('\n')
+        for line in lines:
+            stdscr.addstr(line[:curses.COLS - 1] + '\n')
 
-        while True:
-            if not (0 <= current_page < total_pages):
-                print("Invalid page number.")
-                break
+    stdscr.refresh()
 
-            page = doc[current_page]
-            print(f'Page {current_page + 1} / {total_pages}')
-            print(page.get_text())
+def display_pdf(stdscr, pdf_filename):
+    doc = fitz.open(pdf_filename)
+    total_pages = doc.page_count
+    current_page = 0
+    exit_viewer = False
 
-            print("Press 'b' to go back, 'f' to go forward, 'q' to quit: ", end='', flush=True)
-            
-            event = getch()
-            if platform.system() == 'Windows':
-                choice = event.decode('utf-8').lower()
-            else:
-                choice = event.name
+    stdscr.clear()
+    stdscr.addstr("\n - Additionally, you can use the left arrow key to go back and the right arrow key to go forward.")
+    stdscr.addstr("   Note: Make sure to run the script in a terminal that supports arrow key input,")
+    stdscr.addstr("   and on some platforms, arrow keys might not work in certain terminal environments.\n")
 
-            if choice == 'b':
-                current_page = max(current_page - 1, 0)
-            elif choice == 'f':
-                current_page = min(current_page + 1, total_pages - 1)
-            elif choice == 'q':
-                break
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        if 'doc' in locals():
-            doc.close()
+    while not exit_viewer:
+        display_current_page(stdscr, current_page, total_pages, doc)
+        key = stdscr.getch()
 
-def main():
-    print("""
-    ... (Your welcome message)
-    - Additionally, you can use the left arrow key to go back and the right arrow key to go forward. Note: Make sure to run the script in a terminal that supports arrow key input, and on some platforms, arrow keys might not work in certain terminal environments.
-    """)
+        if key == curses.KEY_RIGHT:
+            current_page = min(current_page + 1, total_pages - 1)
+        elif key == curses.KEY_LEFT:
+            current_page = max(current_page - 1, 0)
+        elif key == ord('q'):
+            exit_viewer = True
+
+    doc.close()
+
+def main(stdscr):
+    curses.curs_set(0)  # Hide cursor
+    stdscr.clear()
+    stdscr.refresh()
+
+    print("\n"
+      "▀▀█▀▀ █▀▀ █▀▀█ █▀▄▀█ ▒█▀▀█ ▒█▀▀▄ ▒█▀▀▀ 　 ▒█░░▒█ ░▀░ █▀▀ █░░░█ █▀▀ █▀▀█\n"
+      "░▒█░░ █▀▀ █▄▄▀ █░▀░█ ▒█▄▄█ ▒█░▒█ ▒█▀▀▀ 　 ░▒█▒█░ ▀█▀ █▀▀ █▄█▄█ █▀▀ █▄▄▀\n"
+      "░▒█░░ ▀▀▀ ▀░▀▀ ▀░░░▀ ▒█░░░ ▒█▄▄▀ ▒█░░░ 　 ░░▀▄▀░ ▀▀▀ ▀▀▀ ░▀░▀░ ▀▀▀ ▀░▀▀\n"
+      "-------------------------------------------------------------------------\n"
+      "TermPDF Viewer - view and navigate PDF files within the terminal.\n"
+      "-------------------------------------------------------------------------\n"
+      "*  Simple: TermPDF Viewer is a Python program that enables users to \n"
+      "*  view and navigate PDF files directly within the terminal.\n"
+      "-------------------------------------------------------------------------\n"
+      "\n\n"
+      "╭────────────-----────── TERMS OF USE ──────────----------───╮\n"
+      "│  This software is licensed under the MIT License.          │\n"
+      "│  By Felipe Alfonso González - github.com/felipealfonsog    │\n"
+      "│  Computer Science Engineer - Email: f.alfonso@res-ear.ch   │\n"
+      "╰───────────────────────────────────────────────---------────╯\n"
+      "\n")
 
     print("Welcome to the TermPDF Viewer!")
     
@@ -147,7 +166,7 @@ def main():
                 try:
                     file_choice = int(file_choice) - 1
                     if 0 <= file_choice < len(pdf_files):
-                        display_pdf(pdf_files[file_choice])
+                        display_pdf(stdscr, pdf_files[file_choice])
                     else:
                         print("Invalid choice.")
                 except ValueError:
@@ -161,4 +180,6 @@ def main():
             print("Invalid choice. Please enter a valid number.")
 
 if __name__ == '__main__':
-    main()
+    curses.wrapper(main)
+
+
